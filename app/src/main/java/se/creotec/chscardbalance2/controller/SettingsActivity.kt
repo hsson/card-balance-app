dd3782a475f551882af4f9886398833e8e5248fa
Alexander Håkansson <al3xand3r.h@gmail.com>
2017-05-30 00:56:42 +0200

    Implements dialog to change food menu language

--- app/src/main/java/se/creotec/chscardbalance2/controller/SettingsActivity.kt
+++ app/src/main/java/se/creotec/chscardbalance2/controller/SettingsActivity.kt
@@ -4,9 +4,12 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import com.afollestad.materialdialogs.MaterialDialog
 import se.creotec.chscardbalance2.Constants
 import se.creotec.chscardbalance2.GlobalState
 import se.creotec.chscardbalance2.R
+import se.creotec.chscardbalance2.util.Util
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -19,21 +22,77 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        val global = application as GlobalState
 
         cardNumberContainer = findViewById(R.id.settings_card_number)
+        cardNumberContainer?.setOnClickListener {
+            Toast.makeText(this, "YO", Toast.LENGTH_LONG).show()
+        }
+
         cardNumberText = findViewById(R.id.settings_card_number_text) as TextView
 
         menuLangContainer = findViewById(R.id.settings_menu_lang)
+        menuLangContainer?.setOnClickListener {
+            MaterialDialog.Builder(this)
+                    .title(R.string.prefs_menu_lang)
+                    .items(R.array.prefs_menu_lang_list)
+                    .itemsCallbackSingleChoice(getDefaultLangIndex(global.model.preferredMenuLanguage), {_, _, which, _ ->
+                        when (which) {
+                            0 -> setMenuLang(Constants.ENDPOINT_MENU_LANG_EN, true)
+                            1 -> setMenuLang(Constants.ENDPOINT_MENU_LANG_SV, true)
+                        }
+                        true
+                    })
+                    .negativeText(R.string.action_cancel)
+                    .show()
+
+        }
         menuLangText = findViewById(R.id.settings_menu_lang_text) as TextView
 
 
 
-        val global = application as GlobalState
-        cardNumberText?.text = global.model.cardData.cardNumber
-        when (global.model.preferredMenuLanguage) {
+        setCardNumber(global.model.cardData.cardNumber)
+        setMenuLang(global.model.preferredMenuLanguage)
+
+
+    }
+
+    private fun setCardNumber(cardNumber: String?, savePreference: Boolean = false) {
+        if (cardNumber == null) {
+            return
+        }
+        val number = Util.formatCardNumber(cardNumber)
+        cardNumberText?.text = number
+        if (savePreference) {
+            // TODO: Save preference
+        }
+    }
+
+    private fun setMenuLang(lang: String?, savePreference: Boolean = false) {
+        if (lang == null) {
+            return
+        }
+        when (lang) {
             Constants.ENDPOINT_MENU_LANG_EN -> menuLangText?.text = getString(R.string.prefs_menu_lang_en)
             Constants.ENDPOINT_MENU_LANG_SV -> menuLangText?.text = getString(R.string.prefs_menu_lang_sv)
+            else -> return
+        }
+        if (savePreference) {
+            val global = application as GlobalState
+            global.model.preferredMenuLanguage = lang
+            global.saveMenuData()
+        }
+    }
+
+    private fun getDefaultLangIndex(lang: String?): Int {
+        if (lang == null) {
+            return -1
         }
 
+        when (lang) {
+            Constants.ENDPOINT_MENU_LANG_EN -> return 0
+            Constants.ENDPOINT_MENU_LANG_SV -> return 1
+            else -> return 0
+        }
     }
 }
