9ab47bba3988e124598220c5dc79d1ba24f4afe7
Alexander Håkansson <al3xand3r.h@gmail.com>
2017-05-29 13:34:58 +0200

    Adds helper function for determining if time is in interval

--- app/src/main/java/se/creotec/chscardbalance2/util/Util.java
+++ app/src/main/java/se/creotec/chscardbalance2/util/Util.java
@@ -4,6 +4,9 @@
 // https://opensource.org/licenses/MIT
 package se.creotec.chscardbalance2.util;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public final class Util {
     private Util() {}
 
@@ -29,6 +32,11 @@ public final class Util {
         return sb.toString();
     }
 
+    /**
+     * Capitalize the first letter of all words in the given string
+     * @param in The string to format
+     * @return The input string with all first letters capitalized
+     */
     public static String capitalizeAllWords(String in) {
         if (in == null  || in.equals("")) {
             return in;
@@ -45,5 +53,29 @@ public final class Util {
             res.append(str).append(" ");
         }
         return res.toString().trim();
+    }
+
+    /**
+     * Determine if the time is currently between two given hours and minutes.
+     * @param from A time of the day on the format where i.e. 800 is 8.00 am, and 2330 is 11.30 pm
+     * @param to A time of the day on the format where i.e. 800 is 8.00 am, and 2330 is 11.30 pm
+     * @return A boolean indicating if the time of the day is between the given interval
+     */
+    public static boolean isBetweenHours(int from, int to) {
+        return isBetweenHours(new Date(), from, to);
+    }
+
+    /**
+     * Determine if the given time is between two given hours and minutes.
+     * @param date The date to check if between interval.
+     * @param from A time of the day on the format where i.e. 800 is 8.00 am, and 2330 is 11.30 pm
+     * @param to A time of the day on the format where i.e. 800 is 8.00 am, and 2330 is 11.30 pm
+     * @return A boolean indicating if the time of the day is between the given interval
+     */
+    public static boolean isBetweenHours(Date date, int from, int to) {
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        int t = c.get(Calendar.HOUR_OF_DAY) * 100 + c.get(Calendar.MINUTE);
+        return to > from && t >= from && t <= to || to < from && (t >= from || t <= to);
     }
 }
