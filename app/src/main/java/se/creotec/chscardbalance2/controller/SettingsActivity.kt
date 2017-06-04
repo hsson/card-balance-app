8a51f6bb6a43f7d49973d948ebf65a71efd39a69
Alexander Håkansson <al3xand3r.h@gmail.com>
2017-06-04 13:07:43 +0200

    Adds license headers

--- app/src/main/java/se/creotec/chscardbalance2/controller/SettingsActivity.kt
+++ app/src/main/java/se/creotec/chscardbalance2/controller/SettingsActivity.kt
@@ -1,3 +1,7 @@
+// Copyright (c) 2017 Alexander Håkansson
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
 package se.creotec.chscardbalance2.controller
 
 import android.content.Intent
